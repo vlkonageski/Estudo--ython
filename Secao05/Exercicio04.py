702ba18eab75b2a5192eee040b7aa7646e183283
vlkonageski <vinicius_konageski@hotmail.com>
2021-11-16 15:55:01 -0300

    resolução

--- Secao05/Exercicio04.py
+++ Secao05/Exercicio04.py
@@ -3,3 +3,13 @@ Faça um programa que leia um numero e, caso ele seja positivo, calcule e mostre
 º O numero digitado ao quadrado
 º A raiz quadrada do numero digitado.
 """
+import math
+
+n = int(input('Informe um numero:'))
+
+if n >= 0:
+    quadrado = n * n
+    print('O quadrado de {} e {}'.format(n, quadrado))
+else:
+    raiz = math.sqrt(n)
+    print('A raiz quadrada de {} e {:.2f}'.format(n, raiz))
