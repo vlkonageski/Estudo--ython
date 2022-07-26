d340928e4f6bec57ad16579678118237ab595170
vlkonageski <vinicius_konageski@hotmail.com>
2022-07-25 21:22:37 -0300

    ajustes do codigo

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -10,16 +10,20 @@ chute = int(input("Chute um numero de 1 a 100:"))
 tentativas = 0
 
 while chute != numero_secreto:
-    if chute < numero_secreto: 
+    if chute < 1 or chute > 100:
+        print("Voce deve chutar um numero entre 1 e 100!")
+        tentativas += 1
+        chute = int(input("Chute um novo numero de 1 a 100:"))
+    elif chute < numero_secreto: 
         print("O numero que voce chutou e menor!")
         chute = int(input("Chute um novo numero de 1 a 100:"))
         tentativas += 1
-    else:
+    elif chute > numero_secreto:
         print("O numero que voce chutou e maior!")
         chute = int(input("Chute um numero de 1 a 100:"))
         tentativas += 1
 
 tentativas += 1
-print("Você acertou, o numero era {:d} e precisou de {:d} tentativas para acertar!".format(numero_secreto, tentativas))
-
+print("Você acertou, o numero era {} e precisou de {} tentativas para acertar!".format(numero_secreto, tentativas))
 print("********Fim de jogo **********")
+
