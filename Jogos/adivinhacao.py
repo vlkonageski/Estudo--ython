4fa795aece61637ccd3072707669196b915c8f5e
vlkonageski <vinicius_konageski@hotmail.com>
2022-07-27 15:49:33 -0300

    ajustes de codigo

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -15,7 +15,6 @@ dificuldade = int(input("Informe a dificuldade:"))
 tentativas = 0
 tentativas_restantes = 1
 
-<<<<<<< HEAD
 if dificuldade == 1:
     tentativas = 20
 elif dificuldade == 2:
@@ -31,18 +30,9 @@ chute = int(input("Chute um numero de 1 a 100:"))
 while tentativas_restantes != tentativas:
     if chute < numero_secreto: 
         tentativas_restantes += 1
-=======
-while chute != numero_secreto:
-    if chute < 1 or chute > 100:
-        print("Voce deve chutar um numero entre 1 e 100!")
-        tentativas += 1
-        chute = int(input("Chute um novo numero de 1 a 100:"))
-    elif chute < numero_secreto: 
->>>>>>> d340928e4f6bec57ad16579678118237ab595170
         print("O numero que voce chutou e menor!")
         print("Tentiva {} de {}.".format(tentativas_restantes, tentativas))
         chute = int(input("Chute um novo numero de 1 a 100:"))
-<<<<<<< HEAD
     elif chute > numero_secreto:
         tentativas_restantes += 1
         print("O numero que voce chutou e maior!")
@@ -52,15 +42,3 @@ while chute != numero_secreto:
         print("Você acertou, o numero era {:d} e precisou de {:d} tentativas para acertar!".format(numero_secreto, tentativas_restantes))
         print("********Fim de jogo **********")
         break
-=======
-        tentativas += 1
-    elif chute > numero_secreto:
-        print("O numero que voce chutou e maior!")
-        chute = int(input("Chute um numero de 1 a 100:"))
-        tentativas += 1
-
-tentativas += 1
-print("Você acertou, o numero era {} e precisou de {} tentativas para acertar!".format(numero_secreto, tentativas))
-print("********Fim de jogo **********")
-
->>>>>>> d340928e4f6bec57ad16579678118237ab595170
