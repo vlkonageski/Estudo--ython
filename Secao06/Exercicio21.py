20509454f96dd5544a65b005d18413bf97415adb
vlkonageski <vinicius_konageski@hotmail.com>
2022-08-02 21:28:02 -0300

    resolucao de exercicios

--- Secao06/Exercicio21.py
+++ Secao06/Exercicio21.py
@@ -3,3 +3,19 @@ Faça um programa que receba dois numeros. Calcule e mostre:
     ° a soma dos numeros pares desse intervalo de numeros, incluindo os numeros digitados.
     ° a multiplicação dos numeros impares desse intervalo, incluindo os digitados.
 """
+
+n1 = int(input("Informe o primeiro numero:"))
+n2 = int(input("Informe o segundo numero:"))
+soma = 0
+multiplicacao = 0
+
+
+while n1 <= n2:
+    if n1 % 2 == 0:
+        soma += n1
+        n1 += 1
+    else:
+        multiplicacao = n1 * n1
+        n1 += 1
+
+print("A soma dos numeros pares e {} e a multiplicacao dos numeros impares e {}!" .format(soma, multiplicacao))
--- Secao06/Exercicio22.py
+++ Secao06/Exercicio22.py
@@ -4,3 +4,17 @@ Escreva um programa completo que permita a qualquer aluno introduzir, pelo tecla
 de notas com que o aluno prentende efetuar o calculo nao sera fornecido ao programa, o qual terminara quando for
 introduzido um valor que nao seja valido como nota de aprovação.
 """
+
+nome = input("Informe o nome do Aluno:")
+notas = float(input("Informe a nota:"))
+qtd_notas = 0
+lista = []
+
+while notas >= 10 and notas <= 20:
+    qtd_notas += 1
+    lista.append(notas)
+    notas = int(input("Informe a nota:"))
+
+media = sum(notas) / qtd_notas
+
+print("A media do Aluno {} foi {}!".format(nome, media))
