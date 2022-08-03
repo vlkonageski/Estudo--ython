c7693c6949490731d14365d15aeb2699c3788ba4
vlkonageski <vinicius_konageski@hotmail.com>
2022-08-02 22:27:51 -0300

    evoluindo jogo

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -3,6 +3,24 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Informe uma letra:")
+
+        index = 0
+        for letra in palavra_secreta:
+            if chute == letra:
+                print("Encontrei a letra {} na posicao {}.".format(letra, index))
+            index = index + 1
+
+
+        print("jogando")
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
