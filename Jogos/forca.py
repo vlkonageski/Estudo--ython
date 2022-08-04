66877402af05721b0ea524baadbe7cc08061f3be
vlkonageski <vinicius_konageski@hotmail.com>
2022-08-04 14:39:43 -0300

    melhorando o jogo de forca

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -4,22 +4,31 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    #STRIP retira os espaços antes e depois da palavra
+    palavra_secreta = palavra_secreta.strip()
+    #UPPER transforma todas as letras em maiusculo
+    palavra_secreta = palavra_secreta.upper()
 
+    acertos = ["_", "_", "_", "_", "_", "_"]
+    
     enforcou = False
     acertou = False
+
+    print(acertos)
 
     while(not enforcou and not acertou):
 
         chute = input("Informe uma letra:")
-
+        chute = chute.strip()
+        chute = chute.upper()
+        
         index = 0
         for letra in palavra_secreta:
-            if chute == letra:
-                print("Encontrei a letra {} na posicao {}.".format(letra, index))
+            if (chute == letra):
+                acertos[index] = letra
             index = index + 1
 
-
-        print("jogando")
+        print(acertos)
 
     print("Fim do jogo")
 
