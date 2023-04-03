d611693c728d1b2b49ef145bf27e8a6d21ae8251
vlkonageski <30873397+vlkonageski@users.noreply.github.com>
2023-04-03 15:49:58 -0300

    resolução de exercicios

--- Secao06/Exercicio23.py
+++ Secao06/Exercicio23.py
@@ -1,3 +1,16 @@
 """
 Faça um algoritimo que leia um numero positivo e imprima seus divisores.
 """
+
+n = int(input("Informe um numero inteiro positivo:"))
+x = n
+lista = []
+
+while x != 0:
+    if n % x == 0:
+        lista.append(x)
+        x -= 1
+    else:
+        x -= 1
+
+print(lista)
